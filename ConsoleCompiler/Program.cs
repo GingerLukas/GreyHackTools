0857f8e36bf52ecad93c03b49d8ebd5f13bcc35f
Lukáš Šafránek <safranek.lukas@ssakhk.cz>
2020-11-29 14:40:30 +0100

    demo updated

--- ConsoleCompiler/Program.cs
+++ ConsoleCompiler/Program.cs
@@ -9,17 +9,30 @@ namespace ConsoleCompiler
     {
         static void Main(string[] args)
         {
-            string s = File.ReadAllText("test.src");
-            int len = s.Length;
-            Stopwatch sw = Stopwatch.StartNew();
-            s = GreyHackCompiler.Compile(s, false);
-            sw.Stop();
-            Console.WriteLine(s);
-            Console.WriteLine(sw.Elapsed);
-            Console.WriteLine(len);
-            Console.WriteLine(s.Length);
-            Console.WriteLine(len-s.Length);
+            string initialCode = File.ReadAllText("Test.src");
+
+            string noOptimize = GreyHackCompiler.Compile(initialCode);
+            string onlyVariables =
+                GreyHackCompiler.Compile(initialCode, true, GreyHackCompiler.Settings.IgnoreMapVariables);
+            string fullOptimize = GreyHackCompiler.Compile(initialCode, true);
+
+            Console.WriteLine($"Initial: {initialCode.Length} chars");
+            Console.WriteLine($"No optimize: {noOptimize.Length} chars {GetPercentage(noOptimize.Length, initialCode.Length)}%");
+            Console.WriteLine($"Optimize w/o maps: {onlyVariables.Length} chars {GetPercentage(onlyVariables.Length, initialCode.Length)}%");
+            Console.WriteLine($"Full optimize: {fullOptimize.Length} chars {GetPercentage(fullOptimize.Length, initialCode.Length)}%");
+
+            Console.WriteLine();
+
+            //Console.WriteLine(noOptimize);
+            //Console.WriteLine(onlyVariables);
+            Console.WriteLine(fullOptimize);
+
             Console.ReadKey();
+        }
+
+        static double GetPercentage(double up, double down)
+        {
+            return (up / down) * 100;
         }
     }
 }
